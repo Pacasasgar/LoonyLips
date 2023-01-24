07e09a23eee270b739a45009df23cb6d0e1fb8ec
Astaroz <pacasasgar@gmail.com>
2023-01-24 19:22:28 +0100

    Importing resources and Main Scene changes

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _ready():
 	print ("Fenomelor!")
+	
+	get_node("DisplayTextKaelthas").text = "Fenomelor!"
+	#otra manera de hacer lo mismo:
+	$DisplayTextKaelthas.text = "Fenomelor? Really?"
