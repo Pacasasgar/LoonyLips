5e5849f0c4e1a143d2b83c43ac501ba3b5138262
Astaroz <pacasasgar@gmail.com>
2023-02-27 18:02:28 +0100

    Now the text displays whatever the player writes

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,9 +1,19 @@
 extends Control
 
-
 func _ready():
 	print ("Fenomelor!")
 	
-	get_node("DisplayTextKaelthas").text = "Fenomelor!"
-	#otra manera de hacer lo mismo:
-	$DisplayTextKaelthas.text = "Fenomelor? Really?"
+	#get_node("DisplayTextKaelthas").text = "Fenomelor!"
+	#una altre manera de fer el mateix:
+	#$DisplayTextKaelthas.text = "Fenomelor? Really?"
+	
+	#Aixó busca els 'nodes' fills amb el nom que indiquem. Però han de ser fills directes.
+	#Amb la estructura de 'nodes' actual, el codi no troba 'DisplayTextKaelthas'. Ho hem de fer així:
+	$VBoxContainer/DisplayTextKaelthas.text = "Fenomelor? Really?" 
+
+func _on_LineEdit_text_entered(new_text):
+	update_DisplayTextKealthas(new_text)
+	
+func update_DisplayTextKealthas(words):
+	$VBoxContainer/DisplayTextKaelthas.text = words
+	$VBoxContainer/LineEdit.clear()
