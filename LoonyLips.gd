1349b7e9072494e6f66b26436a37304dbe16ca70
Astaroz <pacasasgar@gmail.com>
2023-03-07 17:11:01 +0100

    Adding a TextureButton and its logic

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -8,12 +8,16 @@ func _ready():
 	#$DisplayTextKaelthas.text = "Fenomelor? Really?"
 	
 	#Aixó busca els 'nodes' fills amb el nom que indiquem. Però han de ser fills directes.
-	#Amb la estructura de 'nodes' actual, el codi no troba 'DisplayTextKaelthas'. Ho hem de fer així:
+	#Edit1: Amb la estructura de 'nodes' actual, el codi no troba 'DisplayTextKaelthas'. Ho hem de fer així:
 	$VBoxContainer/DisplayTextKaelthas.text = "Fenomelor? Really?" 
 
 func _on_LineEdit_text_entered(new_text):
 	update_DisplayTextKealthas(new_text)
-	
+
+func _on_TextureButton_pressed():
+	var playerWords = $VBoxContainer/HBoxContainer/PlayerText.text
+	update_DisplayTextKealthas(playerWords)
+
 func update_DisplayTextKealthas(words):
 	$VBoxContainer/DisplayTextKaelthas.text = words
-	$VBoxContainer/LineEdit.clear()
+	$VBoxContainer/HBoxContainer/PlayerText.clear()
